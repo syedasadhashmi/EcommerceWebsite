29e55d0cc2ced31e48e54b6dcf1e8d1c5d098cd1
sshashmi <sshashmi@NIS-PK132s-MacBook-Pro.local>
2022-07-04 12:26:17 +0500

    redux persist work implemented

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,10 +3,20 @@ import productsReducer from "./products/productsReducer";
 import cartReducer from "./cart/cartReducer";
 import registrationReducer from "./registration/registrationReducer";
 import { devToolsEnhancer } from "redux-devtools-extension";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "persist-key",
+  storage,
+};
+
 const root = combineReducers({
   productsReducer: productsReducer,
   cartReducer: cartReducer,
   registrationReducer: registrationReducer,
 });
-const store = createStore(root, devToolsEnhancer());
+const persistedReducer = persistReducer(persistConfig, root);
+const store = createStore(persistedReducer, devToolsEnhancer());
+export const persistor = persistStore(store);
 export default store;
