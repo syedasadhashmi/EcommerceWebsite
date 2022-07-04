1f13a614633cb79c0f6e5af485cb5bf5b4152a25
sshashmi <sshashmi@NIS-PK132s-MacBook-Pro.local>
2022-07-04 12:47:36 +0500

    redux-persist provider checked by applying wrapper on indexjs file

--- src/App.js
+++ src/App.js
@@ -5,41 +5,35 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/UI/Navbar";
 import Details from "./components/users/Details";
 import Home from "./pages/Home";
-import { Provider } from "react-redux";
-import store, { persistor } from "./redux/store";
-import { PersistGate } from "redux-persist/integration/react";
+
 import Cart from "./pages/Cart";
 // import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
   return (
     <Router>
-      <Provider store={store}>
-        <PersistGate persistor={persistor}>
-          <div>
-            <Navbar />
-            <div>
-              <Switch>
-                <Route exact path="/">
-                  <Home />
-                </Route>
-                <Route exact path="/Cart">
-                  <Cart />
-                </Route>
-                <Route exact path="/Login">
-                  <Login />
-                </Route>
-                <Route exact path="/Register">
-                  <Register />
-                </Route>
-                <Route exact path="/Details/:id">
-                  <Details />
-                </Route>
-              </Switch>
-            </div>
-          </div>
-        </PersistGate>
-      </Provider>
+      <div>
+        <Navbar />
+        <div>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/Cart">
+              <Cart />
+            </Route>
+            <Route exact path="/Login">
+              <Login />
+            </Route>
+            <Route exact path="/Register">
+              <Register />
+            </Route>
+            <Route exact path="/Details/:id">
+              <Details />
+            </Route>
+          </Switch>
+        </div>
+      </div>
     </Router>
   );
 }
